45e4ade1288032253bb949a2cabbfdf33202d8ba
Stanislav Ra <work.stan@aol.com>
2023-09-16 14:52:27 +0300

    Trade

---
+++ api/app/Http/Controllers/SellStockController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class SellStockController extends Controller
+{
+    public function __construct(private Client $client) {}
+
+    public function get()
+    {
+        $result = $this->client->get("https://datsorange.devteam.games/sellStock",
+            ["headers" =>
+                ["token" => "64ef6461c7ea364ef6461c7ea5"]
+            ]
+        );
+        $response = $result->getBody()->getContents();
+        $response = json_decode($response);
+        $result = [];
+        foreach ($response as $res)
+        {
+            if (count($res['bids']) !== 0)
+            {
+                $result[] = $res;
+            }
+        }
+        dd($result);
+    }
+}
